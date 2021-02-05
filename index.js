279f5db5e1369ebc41307283bd7b13a6700ebd63
Olatunji Odelade <odeladetunji@gmail.com>
2021-02-05 09:43:23 +0100

    testing database connection for deployment

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const circle = require('./routes/circle');
 const fetchToken = require('./routes/fetchToken');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('postgres://postgres:root@localhost:5432/postgres');
 // const swaggerDocument = require('./swagger.json');
 
 const swaggerOptions = {
@@ -130,16 +132,16 @@ server.listen(9000, function(){
 	console.log('Shape and Area Api');
 });
 
-// async function testDb(){
-// 	try {
-// 		await sequelize.authenticate();
-// 		console.log('Connection has been established successfully.');
-// 	  } catch (error) {
-// 		console.error('Unable to connect to the database:', error);
-// 	  }
-// }
+async function testDb(){
+	try {
+		await sequelize.authenticate();
+		console.log('Connection has been established successfully.');
+	  } catch (error) {
+		console.error('Unable to connect to the database:', error);
+	  }
+}
 
-// testDb();
+testDb();
 
 
 module.exports = app;
