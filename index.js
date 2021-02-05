726c4fdeac4f7319c8e227a466ea9d1239d2d437
Olatunji Odelade <odeladetunji@gmail.com>
2021-02-05 04:02:06 +0100

    addition of swagger documentation to the various api

--- index.js
+++ index.js
@@ -11,19 +11,119 @@ const square = require('./routes/square');
 const triangle = require('./routes/triangle');
 const circle = require('./routes/circle');
 const fetchToken = require('./routes/fetchToken');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('./swagger.json');
+
+const swaggerOptions = {
+	swaggerDefinition: {
+		info: {
+			title: "Shapes And Area Api",
+			description: "Basic Api for calculation area of different shapes",
+			contact: {
+				name: 'Olatunji Odelade'
+			},
+			servers: ['http://localhost:9000']
+		}
+	},
+	apis: ['index.js']
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 app.use(bodyParser.urlencoded({ extended: false })); // urlencoded form parser
 app.use(bodyParser.json())  // json parser
 app.use(cors());
 
 //Routes//
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /registerusers:
+ *  post:
+ *    description: for registration of new users
+ *    response: 
+ *     '200':
+ *       description: returns details of the new created user
+ */
 app.use('/registerusers', registerUsers);
+
+/**
+ * @swagger
+ * /fetchAUser:
+ *  get:
+ *    description: endpoint is used to fetch a single user
+ *    response: 
+ *     '200':
+ *       description: returns an object containing a list of all users
+ */
 app.use('/fetchAUser', fetchAUser);
+
+/**
+ * @swagger
+ * /fetchAllUsers:
+ *  get:
+ *    description: endpoint is used to fetched all users in the system
+ *    response: 
+ *     '200':
+ *       description: returns an object containing a list of all users
+ */
 app.use('/fetchAllUsers', fetchAllUsers);
+
+/**
+ * @swagger
+ * /areaOfRectangle:
+ *  post:
+ *    description: endpoint is used to compute the area of a rectangle
+ *    response: 
+ *     '200':
+ *       description: returns details about the computed data of that user
+ */
 app.use('/areaOfRectangle', rectangle);
+
+/**
+ * @swagger
+ * /areaOfSquare:
+ *  post:
+ *    description: endpoint is used to compute the area of a square
+ *    response: 
+ *     '200':
+ *       description: returns details about the computed data of that user
+ */
 app.use('/areaOfSquare', square);
+
+/**
+ * @swagger
+ * /areaOfTriangle:
+ *  post:
+ *    description: endpoint is used to compute the area of a triangle
+ *    response: 
+ *     '200':
+ *       description: returns details about the computed data of that user
+ */
 app.use('/areaOfTriangle', triangle);
+
+/**
+ * @swagger
+ * /areaOfCircle:
+ *  post:
+ *    description: endpoint is used to compute the area of a circle
+ *    response: 
+ *     '200':
+ *       description: returns details about the computed data of that user
+ */
 app.use('/areaOfCircle', circle);
+
+/**
+ * @swagger
+ * /fetchtoken:
+ *  get:
+ *    description: this endpoint is to fetch token
+ *    response: 
+ *     '200':
+ *       description: token returned sucessfully
+ */
 app.use('/fetchtoken', fetchToken);
 
 server.listen(9000, function(){
